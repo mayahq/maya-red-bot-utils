3ac85d791cc20338c54a3c290406bca0f242f907
sibeshkar <sibeshkar@users.noreply.github.com>
2020-07-28 05:28:37 +0530

    intent declaration working

--- nodes/intent.js
+++ nodes/intent.js
@@ -3,33 +3,9 @@ module.exports = function (RED) {
     RED.nodes.createNode(this, config);
     var node = this;
 
-    var group = RED.nodes.getNode(config.group);
-    if (!group) {
-      return;
-    }
-    var tab = RED.nodes.getNode(group.config.tab);
-    if (!tab) {
-      return;
-    }
+    console.log("Testing..");
 
-    console.log(
-      "intent loaded",
-      {
-        node: node,
-        tab: tab,
-        group: group,
-        forwardInputMessages: false,
-        control: {
-          type: "form",
-          label: config.label,
-          order: config.order,
-          value: config.payload || node.id,
-          options: config.options,
-          formValue: config.formValue,
-        },
-      },
-      config.options
-    );
+    console.log(config.options);
   }
   RED.nodes.registerType("bot-intent", IntentNode);
 };
